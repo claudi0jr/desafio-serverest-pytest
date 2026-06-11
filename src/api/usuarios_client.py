1f8d92c75adb189db3c4190af2333d934b8b2b5d
Cláudio Jr. <claudiojr@MacBook-Air-de-Claudio-Jr.local>
2026-06-11 15:03:47 -0300

    fix: corrigido pytest.ini

--- src/api/usuarios_client.py
+++ src/api/usuarios_client.py
@@ -1,25 +1,33 @@
 import requests
 
-BASE_URL = "https://compassuol.serverest.dev"
+ENDPOINT = "https://compassuol.serverest.dev"
+
+#response = requests.get(ENDPOINT)
+#print(response.status_code)
+
+def test_call_endpoint ():
+    response = requests.get(ENDPOINT)
+    assert response.status_code == 200
+    pass
 
 # funções para cada endpoint de usuários
 
 def listar_usuarios():
-    response = requests.get(BASE_URL + "/usuarios")
+    response = requests.get(ENDPOINT + "/usuarios")
     return response
 
 def cadastrar_usuario(dados):
-    response = requests.post(BASE_URL + "/usuarios", json=dados)
+    response = requests.post(ENDPOINT + "/usuarios", json=dados)
     return response
 
 def buscar_usuario(id):
-    response = requests.get(BASE_URL + "/usuarios/" + id)
+    response = requests.get(ENDPOINT + "/usuarios/" + id)
     return response
 
 def atualizar_usuario(id, dados):
-    response = requests.put(BASE_URL + "/usuarios/" + id, json=dados)
+    response = requests.put(ENDPOINT + "/usuarios/" + id, json=dados)
     return response
 
 def excluir_usuario(id):
-    response = requests.delete(BASE_URL + "/usuarios/" + id)
+    response = requests.delete(ENDPOINT + "/usuarios/" + id)
     return response
